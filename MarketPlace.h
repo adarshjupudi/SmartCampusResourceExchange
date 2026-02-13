a96004d1480ee6a941591c709da364acd92e1a05
Adarsh Jupudi <adarsh.jupudi6@gmail.com>
2026-02-13 22:20:28 +0530

    Changes in marketplace.h

--- MarketPlace.h
+++ MarketPlace.h
@@ -10,25 +10,37 @@ class LoanTransaction;
 
 class Marketplace
 {
-    private:
-        std::vector<User*> users;
-        std::vector<Resource*> resources;
-        std::vector<LoanTransaction*> transactions;
+private:
+    std::vector<User*> users;
+    std::vector<Resource*> resources;
+    std::vector<LoanTransaction*> transactions;
 
-        int nextTransactionId;
+    int nextTransactionId;
 
-        public:
-        Marketplace();
+public:
+    Marketplace();
 
-        void addUser(User* user);
-        void addResource(Resource* resource);
+    void addUser(User* user);
+    void addResource(Resource* resource);
 
-        LoanTransaction* requestLoan(User* borrower,Resource* resource,const std::string& startDate,const std::string& dueDate);
+    LoanTransaction* requestLoan(User* borrower,
+                                 Resource* resource,
+                                 const std::string& startDate,
+                                 const std::string& dueDate);
 
-        // getters 
-        const std::vector<User*>& getUsers() const;
-        const std::vector<Resource*>& getResources() const;
-        const std::vector<LoanTransaction*>& getTransactions() const;
-    };
+    // getters 
+    const std::vector<User*>& getUsers() const;
+    const std::vector<Resource*>& getResources() const;
+    const std::vector<LoanTransaction*>& getTransactions() const;
+
+    void loadUsers(const std::string& filename);
+    void saveUsers(const std::string& filename);
+
+    void loadResources(const std::string& filename);
+    void saveResources(const std::string& filename);
+
+    void loadTransactions(const std::string& filename);
+    void saveTransactions(const std::string& filename);
+};
 
 #endif
